5b2b05fd5f09d8b321627bbb8d7fd6a4415bde0a
deliasoare <deliasoare006@yahoo.com>
2023-07-29 13:12:17 +0300

    Add managing functions - gameOver, nextLevel

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,17 +1,35 @@
 import { useState, useEffect } from 'react';
 import CardManager from './CardManager.jsx';
 export default function CardContainer({onChangeLevel, data, level}) {
+    const [currentScore, setCurrentScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
     const [currentData, setCurrentData] = useState([]);
+
+    const gameOver = () => {
+        onChangeLevel(1);
+        setCurrentScore(0);
+    }
+
+    const nextLevel = () => {
+        onChangeLevel(level + 1)
+    }
+
     useEffect(() => {
-        let nrData = level * 5;
         let newData = [];
-        for (let i = 0; i < nrData; i++) {
+        for (let i = (level - 1) * 5; i < (level - 1) * 5 + level * 5; i++) {
             newData[i] = data[i];
         }
         setCurrentData(newData);
     }, [level, data])
 
     return (
-        <CardManager data={currentData[0] !== undefined && currentData} level={level}/>
+        <div className='container'>
+            <div className='stats'>
+                <p className='currentLevel'>Level {level}</p>
+                <p className='currentScore'>Current score: {currentScore}</p>
+                <p className='highScore'>High score: {highScore}</p>
+            </div>
+            <CardManager data={currentData[0] !== undefined && currentData} level={level}/>
+        </div>
     );
 }
