23355ef45a8eb8c9c3e2348e97b998fb06f6337b
deliasoare <deliasoare006@yahoo.com>
2023-07-29 14:54:38 +0300

    Add header structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import Main from './components/Main.jsx';
+import Header from './components/Header';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Main />
       {/* <Footer /> */}
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Card from '../assets/card.png';
+export default function Header() {
+
+    return (
+        <div className='header'>
+            <div className='title'>
+                <img className='logo' src={Card}></img>
+                <div className='name'>Game Memory</div>
+            </div>
+            <div className='helpButtonContainer'>
+                <button className='helpButton'>Help</button>
+            </div>
+        </div>
+    );
+}
