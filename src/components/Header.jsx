10ce6e0a694d017c4b9751a17b542971858f0a5c
deliasoare <deliasoare006@yahoo.com>
2023-07-31 12:09:54 +0300

    Style help modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import Card from '../assets/card.svg';
 export default function Header() {
     const [openHelp, setOpenHelp] = useState(false);
     const helpButtonRef = useRef(null);
+    const helpContainerRef = useRef(null);
     const onOpenHelp = () => {
         helpButtonRef.current.classList.add('clicked');
         setOpenHelp(true);
@@ -25,9 +26,9 @@ export default function Header() {
                 </div>
             </div>
             {openHelp && 
-                <div className='helpContainer' onClick={onCloseHelp}>
+                <div className='helpContainer' ref={helpContainerRef} onClick={(e) => { if (e.target === helpContainerRef.current) onCloseHelp() }}>
                     <div className='helpModal'>
-                        <div className='closeHelpModal'>X</div>
+                        <div className='closeHelpModal' onClick={onCloseHelp}>X</div>
                         <div className='modalContent'>
                             <p><b>WAY OF PLAYING:</b> Select each card only once in order to reach the next level.</p>
                             <p><b>LOSING:</b> You only lose if you select the same card twice; In that case, you are taken back to level 1.</p>
