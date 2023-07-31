4804c22991de5556b5c6278cf82588a018f11325
deliasoare <deliasoare006@yahoo.com>
2023-07-31 20:29:51 +0300

    Change title and add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import logo from '/memoryLogo.png'
 
 import Header from './components/Header';
 import Main from './components/Main';
